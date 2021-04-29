e0849e226bc3b7917be1b0576409032e053725e4
Nhi Nguyen Q. V <nhi.nguyen@supremetech.vn>
2021-04-29 16:22:16 +0700

    first commit

---
+++ precache-manifest.cd4b0def8935eb8ce6a386ae089dba93.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50f657ddf746a81c427d1e161dbb2b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a98f1bab55c5cbe39e0",
+    "url": "/static/css/2.8d3b5be9.chunk.css"
+  },
+  {
+    "revision": "a7ba6977b1c944a59af6",
+    "url": "/static/css/main.b2458426.chunk.css"
+  },
+  {
+    "revision": "6a98f1bab55c5cbe39e0",
+    "url": "/static/js/2.934c721c.chunk.js"
+  },
+  {
+    "revision": "e8c616cf5dfcba4a1ce47da83898e92f",
+    "url": "/static/js/2.934c721c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7ba6977b1c944a59af6",
+    "url": "/static/js/main.191d7d2b.chunk.js"
+  },
+  {
+    "revision": "6c2a21aeab011d7babe4",
+    "url": "/static/js/runtime-main.a5ba5ed3.js"
+  },
+  {
+    "revision": "5650ae6cec790ad2808df3f165146136",
+    "url": "/static/media/apple.5650ae6c.svg"
+  },
+  {
+    "revision": "6c9efe0c8c1fb630cd48d0381ece4297",
+    "url": "/static/media/arrow-left.6c9efe0c.svg"
+  },
+  {
+    "revision": "be237328d9c252b1b9133a02c3be3e5e",
+    "url": "/static/media/arrow-right.be237328.svg"
+  },
+  {
+    "revision": "43e6eb73e3f4911ebaf05cf8b22c8fcc",
+    "url": "/static/media/bangkok.43e6eb73.jpeg"
+  },
+  {
+    "revision": "2194e989e01cd629e5318d5b855a0b97",
+    "url": "/static/media/bg-research.2194e989.jpg"
+  },
+  {
+    "revision": "ea2612aa317a0217b4507f081345229b",
+    "url": "/static/media/calendar.ea2612aa.svg"
+  },
+  {
+    "revision": "3d611cc0b940e00d38e79b80220f3770",
+    "url": "/static/media/califonia.3d611cc0.jpeg"
+  },
+  {
+    "revision": "cd5bd7e800f596fcead731ef01846079",
+    "url": "/static/media/dalat.cd5bd7e8.jpeg"
+  },
+  {
+    "revision": "81d59432cdcaa4a83a3eb8c8c1976c22",
+    "url": "/static/media/danang.81d59432.jpeg"
+  },
+  {
+    "revision": "893e700e08391b7d3c3f1001ac772f98",
+    "url": "/static/media/dubai.893e700e.jpeg"
+  },
+  {
+    "revision": "ea30b60f853ca8dd9023826a56c483c1",
+    "url": "/static/media/facebook.ea30b60f.svg"
+  },
+  {
+    "revision": "48aa304fa508ee4b3435395a9c46962a",
+    "url": "/static/media/google.48aa304f.svg"
+  },
+  {
+    "revision": "0c8e138876e2722b899708ffa35d4462",
+    "url": "/static/media/hanoi.0c8e1388.jpeg"
+  },
+  {
+    "revision": "a01bc0d7a6dcb21dd5376ba03c30569f",
+    "url": "/static/media/loading-white.a01bc0d7.svg"
+  },
+  {
+    "revision": "150490cd1d8ab355336d27a09191bc5c",
+    "url": "/static/media/logo.150490cd.png"
+  },
+  {
+    "revision": "85356947b44f5390aeda35b155667187",
+    "url": "/static/media/moscow.85356947.jpeg"
+  },
+  {
+    "revision": "77708784bd552dab35a5603cf2e1e626",
+    "url": "/static/media/place01.77708784.jpg"
+  },
+  {
+    "revision": "a3bdd26ca9c86020dfe1b051e23a6580",
+    "url": "/static/media/placeholder.a3bdd26c.svg"
+  },
+  {
+    "revision": "16c47e13a9e9bc70f900ba7c6e9981e3",
+    "url": "/static/media/sapa.16c47e13.jpeg"
+  },
+  {
+    "revision": "baf6c7d904f9860cea720c7e333b90f7",
+    "url": "/static/media/singapore.baf6c7d9.jpeg"
+  },
+  {
+    "revision": "819860d1ce123a9ccae75c936d006b59",
+    "url": "/static/media/team.819860d1.svg"
+  },
+  {
+    "revision": "c4f7fdb3afc07ee3504d1144456fe514",
+    "url": "/static/media/vietnam.c4f7fdb3.svg"
+  }
+]);
